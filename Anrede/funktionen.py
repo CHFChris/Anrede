b4547ee54efb2f1911531f8e9e6f511cf30a4366
Chris S <Christian.Schweitzer@rdf.nuernberg.de>
2025-04-24 10:58:40 +0200

    Anrede hinzufügen

--- Anrede/funktionen.py
+++ Anrede/funktionen.py
@@ -1,21 +1,21 @@
 import mariadb
 import sys
 
-try:
-    conn = mariadb.connect(
-        user="Raphi",
-        password="RaphiH",
-        host="localhost",
-        port=3306,
-        database="schlumpfshop3"
-    )
-    
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
- 
- 
-abfrage = conn.cursor()
-anrede = input("Gebe eine neue Anrede ein: ")
-abfrage.execute(f"INSERT INTO anrede(anrede.Anrede) VALUES(\"{anrede}\");")
-conn.commit()
+def verbinde_datenbank():
+    try:
+        conn = mariadb.connect(
+            user="Raphi",
+            password="RaphiH",
+            host="localhost",
+            port=3306,
+            database="schlumpfshop3"
+        )
+        return conn
+    except mariadb.Error as e:
+        print(f"Fehler bei der Verbindung: {e}")
+        sys.exit(1)
+
+def anrede_hinzufuegen(conn, neue_anrede):
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO anrede (Anrede) VALUES (?)", (neue_anrede,))
+    conn.commit()
